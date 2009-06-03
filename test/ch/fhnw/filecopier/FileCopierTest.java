9a5305dcb2f64bcb309ab613a27ffca65836ddce
ronnystandtke <ronnystandtke@039e0273-db16-4845-9e41-ffb582cf4b69>
2009-06-03 15:03:10 +0000

    added test for copying symlinks

--- test/ch/fhnw/filecopier/FileCopierTest.java
+++ test/ch/fhnw/filecopier/FileCopierTest.java
@@ -4,17 +4,17 @@
  * Created on 22. April 2008, 14:21
  *
  * This file is part of the Java File Copy Library.
- * 
+ *
  * The Java File Copy Libraryis free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation; either version 3 of the License,
  * or (at your option) any later version.
- * 
+ *
  * The Java File Copy Libraryis distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -57,6 +57,79 @@ public class FileCopierTest {
         if (!destinationDir.exists() && !destinationDir.mkdir()) {
             fail("could not create destination dir " + destinationDir);
         }
+    }
+
+    /**
+     * test, if we correctly copy symlinks
+     * @throws Exception if an exception occurs
+     */
+    @Test
+    public void testCopySymlink() throws Exception {
+        // create a source file with some content
+        String content = "Please link here.";
+        File normalFile = new File(sourceDir, "normalFile");
+        try {
+            if (!normalFile.createNewFile()) {
+                fail("could not create test file " + normalFile);
+            }
+            FileWriter fileWriter = new FileWriter(normalFile);
+            fileWriter.write(content);
+            fileWriter.flush();
+            fileWriter.close();
+        } catch (IOException ex) {
+            System.out.println("Could not create " + normalFile);
+            throw ex;
+        }
+
+        // create symlink to file
+        String symlinkFileName = "symlink";
+        File symlinkFile = new File(normalFile.getParentFile().getPath() +
+                File.separator + symlinkFileName);
+        ProcessExecutor executor = new ProcessExecutor();
+        int exitValue = executor.executeProcess(
+                "ln", "-s", normalFile.getName(), symlinkFile.getPath());
+        assertEquals("could not create symlink \"" + symlinkFile.getPath() +
+                '\"', 0, exitValue);
+
+        // copy file and symlink
+        CopyJob copyJob = new CopyJob(
+                true, destinationDir.getPath(), sourceDir.getPath() + "/.*");
+        fileCopier.copy(copyJob);
+
+        // check
+        File expectedFile = new File(destinationDir, normalFile.getName());
+        boolean fileExists = expectedFile.exists();
+        boolean fileIsFile = expectedFile.isFile();
+
+        File expectedSymlink = new File(destinationDir, symlinkFileName);
+        boolean symlinkExists = expectedSymlink.exists();
+        boolean symlinkIsFile = expectedSymlink.isFile();
+
+        // cleanup
+        if (!normalFile.delete()) {
+            fail("could not delete source file " + normalFile);
+        }
+        if (!symlinkFile.delete()) {
+            fail("could not delete source symlink " + symlinkFile);
+        }
+        if (!sourceDir.delete()) {
+            fail("could not delete source dir " + sourceDir);
+        }
+        if (!expectedFile.delete()) {
+            fail("could not delete destination file " + expectedFile);
+        }
+        if (!expectedSymlink.delete()) {
+            fail("could not delete destination symlink " + expectedSymlink);
+        }
+        if (!destinationDir.delete()) {
+            fail("could not delete destination dir " + destinationDir);
+        }
+
+        // final check
+        assertTrue("destination file was not created", fileExists);
+        assertTrue("destination file is no file", fileIsFile);
+        assertTrue("destination symlink was not created", symlinkExists);
+        assertFalse("destination symlink is no symlink", symlinkIsFile);
     }
 
     /**
